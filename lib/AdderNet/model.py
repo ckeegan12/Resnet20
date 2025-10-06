e5a7e4999dbb71b1ccb4f5497f79ea97f6cae017
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-10-06 03:42:53 +0000

    add addernet

--- lib/AdderNet/model.py
+++ lib/AdderNet/model.py
@@ -1,1 +1,24 @@
+import torch
+import torch.nn as nn
+from Adder import adder2d_function
 
+class AdderNet(nn.Module):
+
+    def __init__(self,input_channel,output_channel,kernel_size, stride=1, padding=0, bias = False):
+        super(AdderNet, self).__init__()
+        self.stride = stride
+        self.padding = padding
+        self.input_channel = input_channel
+        self.output_channel = output_channel
+        self.kernel_size = kernel_size
+        self.adder = torch.nn.Parameter(nn.init.normal_(torch.randn(output_channel,input_channel,kernel_size,kernel_size)))
+        self.bias = bias
+        if bias:
+            self.b = torch.nn.Parameter(nn.init.uniform_(torch.zeros(output_channel)))
+
+    def forward(self, x):
+        output = adder2d_function(x,self.adder, self.stride, self.padding)
+        if self.bias:
+            output += self.b.unsqueeze(0).unsqueeze(2).unsqueeze(3)
+        
+        return output
