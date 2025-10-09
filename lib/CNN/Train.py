8b03f15983843ee58da160236a1c7dad437a2c25
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-10-09 14:39:30 +0000

    commit

--- lib/CNN/Train.py
+++ lib/CNN/Train.py
@@ -2,26 +2,29 @@ import torch.nn as nn
 import torch.optim as optim
 
 class model_training:
-    def __init__(self, model, lr, train_loader, epochs):
+    def __init__(self, model, train_loader, epochs):
         self.model = model
-        self.lr = lr
         self.train_loader = train_loader
         self.epochs = epochs
 
-        # Optimizer and loss function
-        self.optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.5e-4)
         self.criterion = nn.CrossEntropyLoss()
-
-        self.size = len(train_loader)
 
     def forward(self, device):
         print(f"Start Training for {self.epochs} epochs")
         train_loss_list = []
+        lr = 0.1
+        optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=0.5e-4)
 
         for epoch in range(self.epochs):
-            # print(f"training cycle: {epoch}")
             epoch_losses = []
             self.model.train()
+
+            if epoch // 50:
+                lr = lr * 0.1**(epoch // 50 - 1) 
+                optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=0.5e-4)
+
+            correct = 0
+            total = 0
 
             for i, (images, labels) in enumerate(self.train_loader):
                 images = images.to(device)
@@ -30,21 +33,20 @@ class model_training:
                 # forward pass
                 outputs = self.model(images)
                 loss = self.criterion(outputs, labels)
+                _, predicted = outputs.max(1)
+                total += labels.size(0)
+                correct += predicted.eq(labels).sum().item()
 
                 # back pass
-                self.optimizer.zero_grad()
+                optimizer.zero_grad()
                 loss.backward()
-                self.optimizer.step()
+                optimizer.step()
 
                 epoch_losses.append(loss.item())
-                """
-                if (i % 1000) == 0:
-                    print(f'Epoch {epoch}, Step {i+1}, Loss: {loss.item():.4f}')
-                """
 
             # Calculate average for this epoch only
             epoch_loss = sum(epoch_losses) / len(epoch_losses)
             train_loss_list.append(epoch_loss)  
-            print(f'Epoch [{epoch+1}/{self.epochs}], Average Loss: {epoch_loss:.4f}')
+            print(f'Epoch [{epoch+1}/{self.epochs}], Average Loss: {epoch_loss:.4f}, Accuracy: {100. * (correct/total):.2f}')
             
         return train_loss_list
