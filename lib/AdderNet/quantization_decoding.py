ba72b9f10dd02c01985b56e9221fc24e433c6d81
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-10-20 23:44:08 +0000

    commit

--- lib/AdderNet/quantization_decoding.py
+++ lib/AdderNet/quantization_decoding.py
@@ -6,13 +6,14 @@ class Quant_decode:
     input: scale = scaling facotr for quantization, quant_tensor = quantized array in int8, original_tensor = fp32 original tensor as array
     output: fp32_tensor = scaled up array from int 8 to fp32, max_error = quantization error
     """
-    
-    fp32_tensor = quant_tensor * scale
+    original_shape = quant_tensor.shape
 
-    fp32_array = fp32_tensor.cpu().numpy().flatten()
+    quant_array = quant_tensor.cpu().numpy().flatten()
     original_array = original_tensor.cpu().numpy().flatten()
 
-    quant_error = fp32_array - original_array
+    scaled_array = quant_array * scale
+    fp32_tensor = scaled_array.reshape(original_shape) 
+    quant_error = scaled_array - original_array
     max_error = np.max(np.abs(quant_error))
 
     return fp32_tensor, max_error
